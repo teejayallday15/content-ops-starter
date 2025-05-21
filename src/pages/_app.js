ea8555cff8137e606e4492346ff457fab08eb4c8
teejayallday15 <tyler.kaine15@gmail.com>
2025-05-20 22:10:32 -0500

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,5 @@
 import '../css/main.css';
-import { useEffect } from 'react';
 
 export default function MyApp({ Component, pageProps }) {
-useEffect(() => {
-const script = document.createElement('script');
-script.src = 'https://app.stammer.ai/static/chatbot/js/chatbubble.js';
-script.setAttribute('data-id', 'eeea7869-fbe3-45d0-a45f-cdbd4ae292d1');
-script.setAttribute('data-domain', 'https://app.stammer.ai');
-script.setAttribute('data-position', 'right');
-document.body.appendChild(script);
-}, []);
-
-return <Component {...pageProps} />;
+    return <Component {...pageProps} />;
 }
